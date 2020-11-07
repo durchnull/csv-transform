945a637b9228196819783350d447da6705e96892
durchnull <david@durchnull.de>
2020-11-07 17:40:51 +0100

    Add examples

---
+++ jobs/actions/AddColumns/example.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    // New column name
+    'Category' => [
+        // New value
+        'Mobility' => [
+            // Value conditions
+            'contains' => [
+                // Header name
+                'Description' => [
+                    'Airline name',
+                ]
+            ],
+            'equals' => [
+                'Account Number' => [
+                    '123456789'
+                ],
+                'Hash' => [
+                    'ce78711bbc86f07be4d85c714040e28c4b74592b'
+                ]
+            ]
+        ],
+    ]
+];
